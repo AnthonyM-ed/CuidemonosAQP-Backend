920fbcfb9c229c6bd30c0d24e9ae19d4633cc343
AnthonyM-ed <tonymiyo132@gmail.com>
2025-06-14 20:55:55 -0500

    Fix: Correcion de nombre

--- mi-backend/routes/safezones.js
+++ mi-backend/routes/safezones.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const safeZoneController = require('../controllers/safezoneController');
-const authenticate = require('../middleware/authenticate');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.get('/', authenticate, safeZoneController.getAllSafeZones);
-router.post('/', authenticate, safeZoneController.createSafeZone);
-router.get('/:id', authenticate, safeZoneController.getSafeZoneById);
-router.put('/:id', authenticate, safeZoneController.updateSafeZone);
+router.get('/', authenticateToken, safeZoneController.getAllSafeZones);
+router.post('/', authenticateToken, safeZoneController.createSafeZone);
+router.get('/:id', authenticateToken, safeZoneController.getSafeZoneById);
+router.put('/:id', authenticateToken, safeZoneController.updateSafeZone);
 
 module.exports = router;
