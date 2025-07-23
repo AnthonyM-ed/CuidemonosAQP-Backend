af83551dc2015676c32b2738041e95a18178e8a4
Jordy Emanuel Ayma Cutipa <jayma@unsa.edu.pe>
2025-07-23 18:25:21 -0500

    (Create zones)Modificacion en los campos estus id y select para categoria; y redireccion a detalle de zona

--- mi-backend/routes/safezones.js
+++ mi-backend/routes/safezones.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const safeZoneController = require('../controllers/safezoneController');
-const authenticateToken = require('../middleware/authenticateToken');
-const multer = require('multer');
-const path = require('path');
+    const express = require('express');
+    const router = express.Router();
+    const safeZoneController = require('../controllers/safezoneController');
+    const authenticateToken = require('../middleware/authenticateToken');
+    const multer = require('multer');
+    const path = require('path');
 
-const { createUploadMiddleware } = require('../controllers/imageController');
-const upload = createUploadMiddleware('photo_url'); 
+    const { createUploadMiddleware } = require('../controllers/imageController');
+    const upload = createUploadMiddleware('photo_url'); 
 
-router.post('/', authenticateToken, upload, safeZoneController.createSafeZone);
-router.get('/', authenticateToken, safeZoneController.getAllSafeZones);
-router.get('/:id', authenticateToken, safeZoneController.getSafeZoneById);
-router.put('/:id', authenticateToken, upload, safeZoneController.updateSafeZone);
+    router.post('/', authenticateToken, upload, safeZoneController.createSafeZone);
+    router.get('/', authenticateToken, safeZoneController.getAllSafeZones);
+    router.get('/:id', authenticateToken, safeZoneController.getSafeZoneById);
+    router.put('/:id', authenticateToken, upload, safeZoneController.updateSafeZone);
 
-module.exports = router;
+    module.exports = router;
